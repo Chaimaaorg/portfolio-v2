4e51ec695d2cf347ff005ad654456e707b1cefdf
Chaimaaorg <chaimaaourgani84@gmail.com>
2025-11-17 12:59:02 +0100

    Update footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 import emoji from "react-easy-emoji";
 
 export default function Footer() {
-
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className="footer-text">
-          {emoji("🚀 Built with passion by Chaimaâ Ourgani")}
-        </p>
-        <p className="footer-text">
-          © {new Date().getFullYear()} Chaimaâ Ourgani. All rights reserved.
+          {emoji("💫 Crafted by Chaimaâ Ourgani © {new Date().getFullYear()}")}
         </p>
       </div>
     </Fade>
